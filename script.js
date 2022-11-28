fe76d0870234eca72a23e2cb37d204ade0daa871
AnotherSoftwareCompany <chibike.dave@gmail.com>
2022-11-28 04:07:19 -0800

    changed map url L.tileLayer

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ if (navigator.geolocation)
       const map = L.map("map").setView(coords, 13);
       //   console.log(map);
 
-      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
