c081580011169effeb7c7addc199ebb7df38612b
AnotherSoftwareCompany <chibike.dave@gmail.com>
2022-12-20 05:20:13 -0800

    creating new workout done

--- script.js
+++ script.js
@@ -181,10 +181,10 @@ class App {
           minWidth: 100,
           autoClose: false,
           closeOnClick: false,
-          className: `${type}-popup`,
+          className: `${workout.type}-popup`,
         })
       )
-      .setPopupContent(workout.distance)
+      .setPopupContent(`${workout.type}`)
       .openPopup();
   }
 }
